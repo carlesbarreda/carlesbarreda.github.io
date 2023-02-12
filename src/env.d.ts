02de88f199f6192a2ab60da5fd88fcf14b07f0c3
Carles Barreda <9409275+carlesbarreda@users.noreply.github.com>
2023-02-12 20:43:20 +0100

    update pwa

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,6 +1,7 @@
 /// <reference path="../.astro/types.d.ts" />
 /// <reference types="astro/client" />
-/// <reference types="vite-plugin-pwa/info" />
 /// <reference types="vite-plugin-pwa/client" />
+/// <reference types="vite-plugin-pwa/info" />
+/// <reference types="vite-plugin-pwa" />
 
 declare module "remark-collapse";
