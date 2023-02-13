d5f5b1c5c4e2cbcf94e83229c1daae6d27cf91ca
Carles Barreda <9409275+carlesbarreda@users.noreply.github.com>
2023-02-13 01:48:09 +0100

    update

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,10 +5,12 @@ module.exports = {
     browser: true,
   },
   extends: ["eslint:recommended", "plugin:astro/recommended"],
+  parser: "@typescript-eslint/parser",
   parserOptions: {
     ecmaVersion: "latest",
     sourceType: "module",
   },
+  plugins: ["@typescript-eslint"],
   overrides: [
     {
       files: ["*.astro"],
@@ -16,8 +18,14 @@ module.exports = {
       parserOptions: {
         parser: "@typescript-eslint/parser",
         extraFileExtensions: [".astro"],
+        sourceType: "module",
       },
-      rules: {},
+      extends: ["plugin:astro/recommended"],
+      rules: {
+        "astro/no-set-text-directive": "error",
+        //"astro/no-unused-css-selector": "error",
+        "astro/prefer-class-list-directive": "error",
+      },
     },
   ],
 };
